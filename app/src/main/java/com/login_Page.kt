6e5df6a6cb62ede84af4a9054b527e5cf386ab5c
GrizzlllllyT26 <kamogelomafuna2000@gmail.com>
2024-01-30 13:05:00 +0000

    User Profile

--- app/src/main/java/com/login_Page.kt
+++ app/src/main/java/com/login_Page.kt
@@ -17,16 +17,43 @@ class login_Page : AppCompatActivity() {
         )
         setContentView(R.layout.activity_login_page)
 
-        val LoginButton = findViewById<Button>(R.id.login)
-        val No_acc = findViewById<TextView>(R.id.NoAcc)
+        //val LoginButton = findViewById<Button>(R.id.login)
+        //val No_acc = findViewById<Button>(R.id.NoAcc)
+        //val Forgot_Password = findViewById<Button>(R.id.Forgot_Pass)
 
-        // Set an OnClickListener for the Button
-        LoginButton.setOnClickListener {
-            val intent = Intent(this, Sign_Up_Page::class.java)
+        // Set to link to the Sign Up Page
+        //LoginButton.setOnClickListener {
+            //val intent = Intent(this, Sign_Up_Page::class.java)
+            //startActivity(intent) // Add this line to start the activity
+        //}
+        // Set to link to the Sign Up Page
+        //No_acc.setOnClickListener {
+           // val intent = Intent(this, Sign_Up_Page::class.java)
+            //startActivity(intent) // Add this line to start the activity
+        //}
+        // Set to link to the Forgot Password Page
+        //Forgot_Password.setOnClickListener {
+            //val intent = Intent(this, Forget_password::class.java)
+            //startActivity(intent) // Add this line to start the activity
+        //}
+        val callSignUp = findViewById<Button>(R.id.login)
+
+        callSignUp.setOnClickListener {
+            val intent = Intent(this@login_Page, Sign_Up_Page::class.java)
+            startActivity(intent)
+        }
+        val No_Acc = findViewById<Button>(R.id.NoAcc)
+
+        No_Acc.setOnClickListener {
+            val intent = Intent(this@login_Page, Sign_Up_Page::class.java)
+            startActivity(intent)
         }
 
-        LoginButton.setOnClickListener {
-            val intent = Intent(this, Sign_Up_Page::class.java)
-        }
+        val Forgot_Password = findViewById<Button>(R.id.Forgot_Pass)
+
+            Forgot_Password.setOnClickListener {
+                val intent = Intent(this@login_Page, Forget_password::class.java)
+                startActivity(intent)
+            }
     }
 }
